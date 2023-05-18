ec5c7a1dd8eb58fdc6dae6ff73e2711f0cdbc0e6
Diablo <80642969+progzone122@users.noreply.github.com>
2023-05-18 13:58:20 +0300

    Antipov 17 fix

--- 17/Antipov/main.cpp
+++ 17/Antipov/main.cpp
@@ -4,68 +4,67 @@
 using namespace std;
 
 int main() {
-SetConsoleCP(65001);
-SetConsoleOutputCP(65001);
+  SetConsoleCP(65001);
+  SetConsoleOutputCP(65001);
 
-int rows, cols;
-cout << "Введіть кількість рядків та стовпців: " << endl;
-cin >> rows >> cols;
+  int rows, cols;
+  cout << "Введіть кількість рядків та стовпців: " << endl;
+  cin >> rows >> cols;
 
-// виділяємо пам'ять для двовимірного динамічного масиву
-int **array = new int *[rows];
-for (int i = 0; i < rows; i++) {
-array[i] = new int[cols];
-}
-
-cout << "Введіть матрицю:" << endl;
-for (int i = 0; i < rows; i++) {
-for (int j = 0; j < cols; j++) {
-cout << "Введіть [" << i << "," << j << "] елемент: ";
-cin >> array[i][j];
-}
-}
-
-cout << "Матриця:" << endl;
-for (int i = 0; i < rows; i++) {
-for (int j = 0; j < cols; j++) {
-cout << array[i][j] << " ";
-}
-cout << endl;
-}
-
-// проходимо по кожному стовпцю матриці
-for (int j = 0; j < cols; j++) {
-int max_elem = array[0][j];
-double sum = 0;
-int count = 0;
-
-// проходимо по кожному елементу в стовпці
-for (int i = 0; i < rows; i++) {
-  if (array[i][j] < 0) {
-    sum += array[i][j];
-    count++;
+  // виділяємо пам'ять для двовимірного динамічного масиву
+  int **array = new int *[rows];
+  for (int i = 0; i < rows; i++) {
+    array[i] = new int[cols];
   }
-  if (array[i][j] > max_elem) {
-    max_elem = array[i][j];
+
+  cout << "Введіть матрицю:" << endl;
+  for (int i = 0; i < rows; i++) {
+    for (int j = 0; j < cols; j++) {
+      cout << "Введіть [" << i << "," << j << "] елемент: ";
+      cin >> array[i][j];
+    }
   }
-}
 
-cout << "Максимальний елемент в стовпці " << j << ": " << max_elem << endl;
-if (count == 0) {
-  cout << "У стовпці " << j << " немає від'ємних елементів" << endl;
-} else {
-  double avg = sum / count;
-  cout << "Середнє арифметичне від'ємних елементів в стовпці " << j
-       << ": " << avg << endl;
-}
+  cout << "Матриця:" << endl;
+  for (int i = 0; i < rows; i++) {
+    for (int j = 0; j < cols; j++) {
+      cout << array[i][j] << " ";
+    }
+    cout << endl;
+  }
 
-}
+  // проходимо по кожному стовпцю матриці
+  for (int j = 0; j < cols; j++) {
+    int max_elem = array[0][j];
+    double sum = 0;
+    int count = 0;
 
-// звільняємо виділену пам'ять
-for (int i = 0; i < rows; i++) {
-delete[] array[i];
-}
-delete[] array;
+    // проходимо по кожному елементу в стовпці
+    for (int i = 0; i < rows; i++) {
+      if (array[i][j] < 0) {
+        sum += array[i][j];
+        count++;
+      }
+      if (array[i][j] > max_elem) {
+        max_elem = array[i][j];
+      }
+    }
 
-return 0;
+    cout << "Максимальний елемент в стовпці " << j << ": " << max_elem << endl;
+    if (count == 0) {
+      cout << "У стовпці " << j << " немає від'ємних елементів" << endl;
+    } else {
+      double avg = sum / count;
+      cout << "Середнє арифметичне від'ємних елементів в стовпці " << j << ": "
+           << avg << endl;
+    }
+  }
+
+  // звільняємо виділену пам'ять
+  for (int i = 0; i < rows; i++) {
+    delete[] array[i];
+  }
+  delete[] array;
+
+  return 0;
 }
